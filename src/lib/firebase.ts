edc1b5c7fddbcf0b7c92af1a3015c7032c574a96
trutune <r.k.truetonemusic@gmail.com>
2026-06-01 15:05:11 +0530

    Refactor Firebase setup to include authentication

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDn59hldXbYdgh-raByJFGNJFJ25FCa-nI",
   authDomain: "pics-bf595.firebaseapp.com",
   projectId: "pics-bf595",
   storageBucket: "pics-bf595.firebasestorage.app",
   messagingSenderId: "39510431121",
-  appId: "1:39510431121:web:1e9319eba8f97fe8b4f630",
-  measurementId: "G-JCYCFQQEBF"
+  appId: "1:39510431121:web:1e9319eba8f97fe8b4f630"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export default app;
